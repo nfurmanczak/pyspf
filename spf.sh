3dc6d3a2805240f714412ec6d9ed9b2d41b4e869
Nikolai Furmanczak <nikolai.furmanczak@gmail.com>
2019-12-31 15:33:15 +0100

    fixed error in grep for -all/?all/~all

--- spf.sh
+++ spf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 domain=$1
+declare -A ipArray
 
 if [ ! -n "$domain" ]; then 
 	echo "Usage: ./spf.sh www.domain.de"
@@ -9,20 +10,21 @@ fi
 
 # 
 if dig -t txt $domain | grep --quiet "v=spf1"; then 
-	echo "SPF-Record found."
+	echo -e "\e[32mSPF recound found:\e[0m"
 else 
-	echo "NO SPF-Record found for Domain $domain"
+	echo -e "\e[31mNo SPF record found for domain:\e[0m $domain" 
 	exit
 fi
 
-spfrr=$(dig -t txt $domain | egrep -o "v=spf1.*")
+# 
+spfrr=$(dig -t txt $domain | egrep -o "v=spf1.*" | tr -d \")
 
 
-# Check if spfrr end with all 
-if echo $spfrr | egrep --quiet " [~-?]{1}all$" ; then
-	echo "all gefunden"
-else 
-	echo "KEIN all gefunden. SPF ungültig"
+# Check if spfrr end with all
+if ! echo $spfrr | egrep --quiet "[?~-]{1}all$" ; then
+	echo "No all mechanisms in SPF record. Please add -all/~all or ?all to this SPF record."
 fi	
+
+# [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/{1}[0-9]{1,2}
 
 echo $spfrr
